6e3464ed1201e8fca2534eecd56077e6300ad9f6
Olgauk <ms.olgasmith.jj@mail.ru>
2026-09-25 22:12:56 +0300

    icons bug

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const basePath = process.env.NEXT_BASE_PATH ?? '';
 
 const nextConfig = {
   output: 'export',
-  basePath,
+  basePath: basePath,
   env: {
     NEXT_PUBLIC_BASE_PATH: basePath,
   },
